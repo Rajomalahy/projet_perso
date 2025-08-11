c02abcc67a4c6c4d1a884d412ab87332dae13ab3
Rajomalahy <nanncyrato@gmail.com>
2025-08-11 13:48:31 +0300

    premier commit

---
+++ src/components/page/Index.jsx
@@ -0,0 +1,27 @@
+
+export default function Index(){
+
+    return(
+        <>
+            <div>
+                <header className="flex gap-7 justify-around">
+                    <div className="flex gap-7 items-center">
+                        <div className="text-2xl text-gray-400 font-bold hover:text-[#232028] cursor-pointer">Shortly</div>
+                        <div className="text-gray-400 font-bold hover:text-[#232028] cursor-pointer">Features</div>
+                        <div className="text-gray-400 font-bold hover:text-[#232028] cursor-pointer">Pricing</div>
+                        <div className="text-gray-400 font-bold hover:text-[#232028] cursor-pointer">Resources</div>
+                    </div>
+                    
+                    <div className="flex gap-7 items-center">
+                        <div className="text-gray-400 font-bold hover:text-[#232028] cursor-pointer">Login</div>
+                        <div className="text-gray-400 font-bold hover:bg-[] cursor-pointer">Sign Up</div>
+                    </div>
+                </header>
+                <section>
+                    <div></div>
+                    <div></div>
+                </section>
+            </div>
+        </>
+    )
+}
